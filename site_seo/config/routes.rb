e2bf858241fb777bee92b22492522ea450f99e94
ThalesMachado <thalesm@id.uff.br>
2018-01-16 22:52:17 -0200

    Se rails e com html simples

--- site_seo/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-
-  root to: 'static_pages#index'
-  get 'home', to: "static_pages#index", as: :home
-  get 'trilhas', to: "static_pages#pagina_2", as: :trilhas
-  get 'eventos', to: "static_pages#pagina_3", as: :eventos
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
